6b9c5a58849534d447ee9f034e3ce90529e5e845
sam <spam@spam.spam>
2016-02-20 00:20:25 -0500

    cleaned
    cleaned up a lot of stuff

--- cis600/Hw3ControllerPart2.js
+++ cis600/Hw3ControllerPart2.js
@@ -65,9 +65,6 @@ var CellularAutomaton = (function () {
     CellularAutomaton.prototype.getB = function () {
         return this.a;
     };
-    CellularAutomaton.prototype.getCurrentRow = function () {
-        return this.currentRow;
-    };
     CellularAutomaton.prototype.makeNewRow = function () {
         var len = this.currentRow.length;
         var new_row = Array(len);
@@ -121,7 +118,7 @@ var Hw3Controllerv2 = (function (_super) {
         var _this = this;
         _super.call(this, elementId);
         this.data = [];
-        this.increment = 0.1;
+        this.increment = 0.01;
         var a = 0;
         var b = 0;
         var data = [];
@@ -154,53 +151,23 @@ var Hw3Controllerv2 = (function (_super) {
         return;
     };
     Hw3Controllerv2.prototype.dostuff = function () {
+        var width = parseInt(this.svg.style("width"));
+        var height = parseInt(this.svg.style("height"));
+        var num_boxes = (1 / this.increment) + 1;
         var heat_data = [];
         for (var i = 0; i < this.data.length; ++i) {
             var ca = this.data[i];
             ca.makeNewRow();
             var entropy = ca.getEntropy();
-            var width = parseInt(this.svg.style("width"));
-            var height = parseInt(this.svg.style("height"));
-            var num_boxes = (1 / this.increment) + 1;
             heat_data.push({
                 value: entropy,
                 x: ca.getA() / this.increment,
                 y: ca.getB() / this.increment,
-                color: "#" + this.toHexString(entropy)
+                color: "#" + gToHexString(entropy)
             });
         }
         this.graphHeatMap(heat_data);
         return;
-    };
-    Hw3Controllerv2.prototype.realMod = function (n, m) {
-        // javascript mod doesnt work with negative numbers
-        //http://stackoverflow.com/a/17323608
-        return ((n % m) + m) % m;
-    };
-    Hw3Controllerv2.prototype.nextRow = function (a, b, row) {
-        var rval = Array(row.length);
-        for (var i = 0; i < row.length; ++i) {
-            var previous = row[this.realMod(i - 1, row.length)];
-            var current = row[this.realMod(i, row.length)];
-            var next = row[this.realMod(i + 1, row.length)];
-            rval[i] = this.poly(a, b, previous, current, next);
-            if (rval[i] > 1) {
-                alert(rval[i]);
-            }
-            else if (rval[i] < 0) {
-                alert(rval[i]);
-            }
-        }
-        return rval;
-    };
-    // taken from Blair's ALife1Dim Java program
-    Hw3Controllerv2.prototype.poly = function (a, b, u, x, v) {
-        return (0.5 - 0.5 * Math.cos(Math.PI * (a + (a - b) * v + b * u * v - 2 * u * x * v)));
-    };
-    Hw3Controllerv2.prototype.toHexString = function (val) {
-        var hexstring = Math.round(val * 0xFFFFFF).toString(16);
-        hexstring = (hexstring.length < 6) ? "000000".substr(hexstring.length - 6) + hexstring : hexstring;
-        return hexstring;
     };
     Hw3Controllerv2.prototype.graphHeatMap = function (data) {
         var total_width = parseInt(this.svg.style("width"));
@@ -222,92 +189,6 @@ var Hw3Controllerv2 = (function (_super) {
             .style("fill", function (d) {
             return d.color;
         });
-    };
-    Hw3Controllerv2.prototype.graphRow = function (row, yIndex) {
-        var total_width = parseInt(this.svg.style("width"));
-        var rec_width = total_width / row.length;
-        var y_index = yIndex * rec_width;
-        var context = this.svg.node().getContext("2d");
-        row.forEach(function (d, i) {
-            context.beginPath();
-            context.rect(i * rec_width, y_index, rec_width, rec_width);
-            context.fillStyle = "#" + Hw3Controller.prototype.toHexString(row[i]);
-            context.fill();
-            //if (row[i] == 0 && context.fillStyle != "#000000") {
-            //    var stop = true;
-            //}
-            //console.log(row[i].toString() + " -> " + context.fillStyle);
-            context.closePath();
-        });
-    };
-    Hw3Controllerv2.prototype.countRowByThree = function (row, counts) {
-        for (var i = 0; i < row.length; ++i) {
-            var value = this.toHexString(row[this.realMod(i, row.length)]) +
-                this.toHexString(row[this.realMod(i + 1, row.length)]) +
-                this.toHexString(row[this.realMod(i + 2, row.length)]);
-            counts[value] = (counts[value] ? counts[value] + 1 : 1);
-        }
-    };
-    Hw3Controllerv2.prototype.printStats = function (row, col, width, stats) {
-        //this.statsBox.selectAll("p").remove();
-        //var info_p = this.statsBox.append("p");
-        //var statstr: string = "row " + row + ", col " + col + " (length " + width + ") with a=" + this.a.toString() + " and b=" + this.b.toString();
-        //info_p.text(statstr);
-        //var count_p = this.statsBox.append("p");
-        ////count_p.text("count: " + JSON.stringify(stats, null, 2));
-        //var count_array = [];
-        //for (var c in stats) {
-        //    count_array.push([c, stats[c]]);
-        //}
-        //count_array.sort(function (a, b) {
-        //    return a[1] - b[1];
-        //});
-        //count_array.reverse();
-        //statstr = "count: { ";
-        //for (var i = 0; i < count_array.length; ++i) {
-        //    statstr += '"' + count_array[i][0].toString() + '": ' + count_array[i][1].toString();
-        //    if (i < count_array.length - 1) {
-        //        statstr += ", "
-        //    }
-        //}
-        //statstr += " }"
-        //count_p.text(statstr);
-        //var total_count: number = 0;
-        //var weights: { [color: string]: number } = {};
-        //var entropy: number = 0;
-        //var valid_strings = Object.keys(stats);
-        //for (var i: number = 0; i < valid_strings.length; ++i) {
-        //    var count = stats[valid_strings[i]];
-        //    var weight = count / row;
-        //    weights[valid_strings[i]] = weight;
-        //    entropy += weight * (Math.log(weight) / Math.log(2));
-        //    total_count += count;
-        //}
-        //if (total_count != row) {
-        //    this.statsBox.append("p").text("total count != row:  " + total_count.toString() + " != " + row.toString());
-        //    alert("totalcount != row");
-        //}
-        //var fract_p = this.statsBox.append("p");
-        //statstr = "weights: [";
-        //valid_strings = Object.keys(weights);
-        //for (var i: number = 0; i < valid_strings.length; ++i) {
-        //    var weight = weights[valid_strings[i]];
-        //    statstr += weight.toFixed(3);
-        //    if (i < valid_strings.length - 1) {
-        //        statstr += ",";
-        //    }
-        //}
-        //statstr += "]";
-        ////fract_p.text(statstr);
-        //fract_p.text("weights: " + JSON.stringify(weights, function (key, value) {
-        //    if (key) {
-        //        return value.toFixed(3);
-        //    }
-        //    return value;
-        //}, 2));
-        //var entropy_p = this.statsBox.append("p");
-        //entropy_p.text("shannon's entropy: " + (0 - entropy));
-        //return;
     };
     return Hw3Controllerv2;
 })(BaseTimer);
--- cis600/Hw3ControllerPart2.ts
+++ cis600/Hw3ControllerPart2.ts
@@ -70,10 +70,6 @@ class CellularAutomaton {
         return this.a;
     }
 
-    getCurrentRow(): number[] {
-        return this.currentRow;
-    }
-
     public makeNewRow() {
         var len: number = this.currentRow.length;
         var new_row: number[] = Array(len);
@@ -126,7 +122,7 @@ class Hw3Controllerv2 extends BaseTimer {
     svg: any;
     statsBox: any;
     data: CellularAutomaton[] = [];
-    increment: number = 0.1;
+    increment: number = 0.01;
 
     constructor(elementId: string) {
         super(elementId);
@@ -172,66 +168,31 @@ class Hw3Controllerv2 extends BaseTimer {
     }
 
     dostuff() {
+        const width: number = parseInt(this.svg.style("width"));
+        const height: number = parseInt(this.svg.style("height"));
+        const num_boxes = (1 / this.increment) + 1;
         var heat_data: { value: number, x: number, y: number, color: string }[] = [];
         for (var i: number = 0; i < this.data.length; ++i) {
             var ca: CellularAutomaton = this.data[i];
             ca.makeNewRow();
-
             var entropy: number = ca.getEntropy();
-            var width = parseInt(this.svg.style("width"));
-            var height = parseInt(this.svg.style("height"));
-            var num_boxes = (1 / this.increment) + 1;
             heat_data.push({
                 value: entropy,
                 x: ca.getA() / this.increment,
                 y: ca.getB() / this.increment,
-                color: "#" + this.toHexString(entropy)
+                color: "#" + gToHexString(entropy)
             });
         }
         this.graphHeatMap(heat_data);
         return;
     }
 
-    realMod(n: number, m: number): number {
-        // javascript mod doesnt work with negative numbers
-        //http://stackoverflow.com/a/17323608
-        return ((n % m) + m) % m;
-    }
-
-    nextRow(a: number, b: number, row: number[]): number[] {
-        var rval: number[] = Array(row.length);
-        for (var i: number = 0; i < row.length; ++i) {
-            var previous: number = row[this.realMod(i - 1, row.length)];
-            var current: number = row[this.realMod(i, row.length)];
-            var next: number = row[this.realMod(i + 1, row.length)];
-            rval[i] = this.poly(a, b, previous, current, next);
-            if (rval[i] > 1) {
-                alert(rval[i]);
-            }
-            else if (rval[i] < 0) {
-                alert(rval[i]);
-            }
-        }
-        return rval;
-    }
-
-    // taken from Blair's ALife1Dim Java program
-    poly(a: number, b: number, u: number, x: number, v: number): number {
-        return (0.5 - 0.5 * Math.cos(Math.PI * (a + (a - b) * v + b * u * v - 2 * u * x * v)));
-    }
-
-    toHexString(val: number): string {
-        var hexstring: string = Math.round(val * 0xFFFFFF).toString(16);
-        hexstring = (hexstring.length < 6) ? "000000".substr(hexstring.length - 6) + hexstring : hexstring;
-        return hexstring;
-    }
-
     graphHeatMap(data: { value: number, x: number, y: number, color: string }[]) {
-        var total_width = parseInt(this.svg.style("width"));
-        var total_height = parseInt(this.svg.style("height"));
+        const total_width: number = parseInt(this.svg.style("width"));
+        const total_height: number = parseInt(this.svg.style("height"));
+        const num_boxes = (1 / this.increment) + 1;
+        const size: number = total_width / num_boxes;
 
-        var num_boxes = (1 / this.increment) + 1;
-        var size: number = total_width / num_boxes;
         var rects = this.svg.selectAll("rect")
             .data(data, function (d, i) { return d.value; });
         rects.exit().remove();
@@ -247,103 +208,5 @@ class Hw3Controllerv2 extends BaseTimer {
             .style("fill", function (d) {
                 return d.color;
             });
-    }
-
-    graphRow(row: number[], yIndex: number) {
-        var total_width = parseInt(this.svg.style("width"));
-        var rec_width = total_width / row.length;
-        var y_index = yIndex * rec_width;
-        var context = this.svg.node().getContext("2d");
-
-        row.forEach(function (d, i) {
-            context.beginPath();
-            context.rect(i * rec_width, y_index, rec_width, rec_width);
-            context.fillStyle = "#" + Hw3Controller.prototype.toHexString(row[i]);
-            context.fill();
-            //if (row[i] == 0 && context.fillStyle != "#000000") {
-            //    var stop = true;
-            //}
-            //console.log(row[i].toString() + " -> " + context.fillStyle);
-            context.closePath();
-        });
-    }
-
-    countRowByThree(row: number[], counts: { color: string, number }[]) {
-        for (var i: number = 0; i < row.length; ++i) {
-            var value: string =
-                this.toHexString(row[this.realMod(i, row.length)]) +
-                this.toHexString(row[this.realMod(i + 1, row.length)]) +
-                this.toHexString(row[this.realMod(i + 2, row.length)]);
-            counts[value] = (counts[value] ? counts[value] + 1 : 1);
-        }
-    }
-
-    printStats(row: number, col: number, width: number, stats: { [color: string]: number }) {
-        //this.statsBox.selectAll("p").remove();
-
-        //var info_p = this.statsBox.append("p");
-        //var statstr: string = "row " + row + ", col " + col + " (length " + width + ") with a=" + this.a.toString() + " and b=" + this.b.toString();
-        //info_p.text(statstr);
-
-        //var count_p = this.statsBox.append("p");
-        ////count_p.text("count: " + JSON.stringify(stats, null, 2));
-        //var count_array = [];
-        //for (var c in stats) {
-        //    count_array.push([c, stats[c]]);
-        //}
-        //count_array.sort(function (a, b) {
-        //    return a[1] - b[1];
-        //});
-        //count_array.reverse();
-        //statstr = "count: { ";
-        //for (var i = 0; i < count_array.length; ++i) {
-        //    statstr += '"' + count_array[i][0].toString() + '": ' + count_array[i][1].toString();
-        //    if (i < count_array.length - 1) {
-        //        statstr += ", "
-        //    }
-        //}
-        //statstr += " }"
-        //count_p.text(statstr);
-
-        //var total_count: number = 0;
-        //var weights: { [color: string]: number } = {};
-        //var entropy: number = 0;
-
-        //var valid_strings = Object.keys(stats);
-        //for (var i: number = 0; i < valid_strings.length; ++i) {
-        //    var count = stats[valid_strings[i]];
-        //    var weight = count / row;
-        //    weights[valid_strings[i]] = weight;
-        //    entropy += weight * (Math.log(weight) / Math.log(2));
-        //    total_count += count;
-        //}
-        //if (total_count != row) {
-        //    this.statsBox.append("p").text("total count != row:  " + total_count.toString() + " != " + row.toString());
-        //    alert("totalcount != row");
-        //}
-
-        //var fract_p = this.statsBox.append("p");
-        //statstr = "weights: [";
-        //valid_strings = Object.keys(weights);
-        //for (var i: number = 0; i < valid_strings.length; ++i) {
-        //    var weight = weights[valid_strings[i]];
-        //    statstr += weight.toFixed(3);
-        //    if (i < valid_strings.length - 1) {
-        //        statstr += ",";
-        //    }
-        //}
-        //statstr += "]";
-        ////fract_p.text(statstr);
-        //fract_p.text("weights: " + JSON.stringify(weights, function (key, value) {
-        //    if (key) {
-        //        return value.toFixed(3);
-        //    }
-        //    return value;
-        //}, 2));
-
-        //var entropy_p = this.statsBox.append("p");
-        //entropy_p.text("shannon's entropy: " + (0 - entropy));
-
-        //return;
     }
 }
