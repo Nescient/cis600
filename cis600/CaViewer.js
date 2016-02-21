14a0b6fcb91ef21551a425af6875b4f881b932e8
sam <spam@spam.spam>
2016-02-20 21:04:26 -0500

    added viewer
    added a viewer to show what a ca looks like

---
+++ cis600/CaViewer.js
@@ -0,0 +1,86 @@
+// Copyright © Sam Savage 2016
+var CaViewer = (function () {
+    function CaViewer(svg, rowLength, a, b) {
+        this.currentRow = [];
+        this.data = [];
+        this.a = Math.random();
+        this.b = Math.random();
+        this.boxSize = 0;
+        this.height = 0;
+        this.timerToken = 0;
+        this.timerTimeout = 10;
+        for (var i = 0; i < rowLength; ++i) {
+            this.currentRow.push(Math.random());
+        }
+        this.a = a;
+        this.b = b;
+        this.svg = svg;
+        this.height = parseInt(this.svg.style("height"));
+        this.boxSize = parseInt(this.svg.style("width")) / rowLength;
+        this.graphRow(this.currentRow, 0);
+        return;
+    }
+    CaViewer.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () { return _this.timeStep(); }, this.timerTimeout);
+        return;
+    };
+    CaViewer.prototype.stop = function () {
+        clearInterval(this.timerToken);
+        return;
+    };
+    CaViewer.prototype.timeStep = function () {
+        var next_row = this.nextRow(this.currentRow);
+        this.data.push(this.currentRow);
+        this.graphRow(next_row, this.data.length);
+        this.currentRow = next_row;
+        return;
+    };
+    CaViewer.prototype.nextRow = function (row) {
+        var rval = Array(row.length);
+        for (var i = 0; i < row.length; ++i) {
+            var previous = row[gRealMod(i - 1, row.length)];
+            var current = row[gRealMod(i, row.length)];
+            var next = row[gRealMod(i + 1, row.length)];
+            rval[i] = this.poly(previous, current, next);
+            if (rval[i] > 1) {
+                alert(rval[i]);
+            }
+            else if (rval[i] < 0) {
+                alert(rval[i]);
+            }
+        }
+        return rval;
+    };
+    // taken from Blair's ALife1Dim Java program
+    CaViewer.prototype.poly = function (u, x, v) {
+        return (0.5 - 0.5 * Math.cos(Math.PI * (this.a + (this.a - this.b) * v + this.b * u * v - 2 * u * x * v)));
+    };
+    //public addNewRow(row: number[]) {
+    //    this.data.push(this.currentRow);
+    //    this.graphRow(row, this.data.length);
+    //    this.currentRow = row;
+    //    return;
+    //}
+    CaViewer.prototype.graphRow = function (row, yIndex) {
+        var box_size = this.boxSize;
+        var y_index = yIndex * this.boxSize;
+        var context = this.svg.node().getContext("2d");
+        if (y_index > this.height) {
+            context.translate(0, 1);
+        }
+        row.forEach(function (d, i) {
+            context.beginPath();
+            context.rect(i * box_size, y_index, box_size, box_size);
+            context.fillStyle = "#" + gToHexString(row[i]);
+            context.fill();
+            context.closePath();
+        });
+        //var destCtx = destinationCanvas.getContext('2d');
+        //call its drawImage() function passing it the source canvas directly
+        //destCtx.drawImage(sourceCanvas, 0, 0);
+        return;
+    };
+    return CaViewer;
+})();
+//# sourceMappingURL=CaViewer.js.map
---
+++ cis600/CaViewer.ts
@@ -0,0 +1,92 @@
+﻿// Copyright © Sam Savage 2016
+
+class CaViewer {
+    svg: any;
+    statsBox: any;
+    currentRow: number[] = [];
+    data: number[][] = [];
+    a: number = Math.random();
+    b: number = Math.random();
+    boxSize: number = 0;
+    height: number = 0;
+    timerToken: number=0;
+    timerTimeout: number=10;
+
+    constructor(svg: any, rowLength: number, a: number, b: number) {
+        for (var i: number = 0; i < rowLength; ++i) {
+            this.currentRow.push(Math.random());
+        }
+        this.a = a;
+        this.b = b;
+        this.svg = svg;
+        this.height = parseInt(this.svg.style("height"));
+        this.boxSize = parseInt(this.svg.style("width")) / rowLength;
+        this.graphRow(this.currentRow, 0);
+        return;
+    }
+
+    public start() {
+        this.timerToken = setInterval(() => this.timeStep(), this.timerTimeout);
+        return;
+    }
+
+    public stop() {
+        clearInterval(this.timerToken);
+        return;
+    }
+
+    public timeStep() {
+        var next_row: number[] = this.nextRow(this.currentRow);
+        this.data.push(this.currentRow);
+        this.graphRow(next_row, this.data.length);
+        this.currentRow = next_row;
+        return;
+    }
+
+    private nextRow(row: number[]): number[] {
+        var rval: number[] = Array(row.length);
+        for (var i: number = 0; i < row.length; ++i) {
+            var previous: number = row[gRealMod(i - 1, row.length)];
+            var current: number = row[gRealMod(i, row.length)];
+            var next: number = row[gRealMod(i + 1, row.length)];
+            rval[i] = this.poly(previous, current, next);
+            if (rval[i] > 1) { alert(rval[i]); }
+            else if (rval[i] < 0) { alert(rval[i]); }
+        }
+        return rval;
+    }
+
+    // taken from Blair's ALife1Dim Java program
+    private poly(u: number, x: number, v: number): number {
+        return (0.5 - 0.5 * Math.cos(Math.PI * (this.a + (this.a - this.b) * v + this.b * u * v - 2 * u * x * v)));
+    }
+
+    //public addNewRow(row: number[]) {
+    //    this.data.push(this.currentRow);
+    //    this.graphRow(row, this.data.length);
+    //    this.currentRow = row;
+    //    return;
+    //}
+
+    private graphRow(row: number[], yIndex: number) {
+        const box_size: number = this.boxSize;
+        const y_index: number = yIndex * this.boxSize;
+        var context = this.svg.node().getContext("2d");
+        if (y_index > this.height) {
+            context.translate(0, 1);
+        }
+        row.forEach(function (d, i) {
+            context.beginPath();
+            context.rect(i * box_size, y_index, box_size, box_size);
+            context.fillStyle = "#" + gToHexString(row[i]);
+            context.fill();
+            context.closePath();
+        });
+
+        //var destCtx = destinationCanvas.getContext('2d');
+
+        //call its drawImage() function passing it the source canvas directly
+        //destCtx.drawImage(sourceCanvas, 0, 0);
+        return;
+    }
+}
